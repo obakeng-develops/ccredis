97decc9426c23e76642301682af0c65844c6b485
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-09-02 16:28:41 +0200

    Add tests to deserialize data types

--- pkg/deserializer_test.go
+++ pkg/deserializer_test.go
@@ -1,6 +1,7 @@
 package pkg_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/obakeng-develops/redis-server/pkg"
@@ -30,6 +31,117 @@ func TestDeserializeSimplesStrings(t *testing.T) {
 
 			if result != tc.expected {
 				t.Errorf("got %s, want %s", result, tc.expected)
+			}
+		})
+	}
+}
+
+func TestDeserializeErrors(t *testing.T) {
+	tests := []struct {
+		description string
+		input       string
+		expected    string
+	}{
+		{
+			description: "returns unknown command",
+			input:       "-unknown command\r\n",
+			expected:    "unknown command",
+		},
+		{
+			description: "returns operation against wrong type",
+			input:       "-operation against wrong type\r\n",
+			expected:    "operation against wrong type",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			result := pkg.DeserializeErrors(tc.input)
+
+			if result != tc.expected {
+				t.Errorf("got %s, want %s", result, tc.expected)
+			}
+		})
+	}
+}
+
+func TestDeserializeIntegers(t *testing.T) {
+	tests := []struct {
+		description string
+		input       string
+		expected    int
+	}{
+		{
+			description: "returns 100",
+			input:       ":100\r\n",
+			expected:    100,
+		},
+		{
+			description: "returns 10",
+			input:       ":10\r\n",
+			expected:    10,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			result := pkg.DeserializeIntegers(tc.input)
+
+			if result != tc.expected {
+				t.Errorf("got %v, want %v", result, tc.expected)
+			}
+		})
+	}
+}
+
+func TestDeserializeBulkStrings(t *testing.T) {
+	tests := []struct {
+		description string
+		input       string
+		expected    string
+	}{
+		{
+			description: "returns hello",
+			input:       "$5\r\nhello\r\n",
+			expected:    "hello",
+		},
+		{
+			description: "returns 10",
+			input:       "$0\r\n\r\n",
+			expected:    "",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			result := pkg.DeserializeBulkStrings(tc.input)
+
+			if result != tc.expected {
+				t.Errorf("got %v, want %v", result, tc.expected)
+			}
+		})
+	}
+}
+
+func TestDeserializeArrays(t *testing.T) {
+	tests := []struct {
+		description string
+		input       string
+		expected    interface{}
+	}{
+		{
+			description: "returns 3 integer arrary",
+			input:       "*3\r\n:1\r\n:2\r\n:3\r\n",
+			expected:    []int{1, 2, 3},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			result := pkg.DeserializeArrays(tc.input)
+
+			if !reflect.DeepEqual(result, tc.expected) {
+				t.Errorf("got %v, want %v", result, tc.expected)
 			}
 		})
 	}
