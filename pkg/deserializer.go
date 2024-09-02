184e2f9859128efab5ab7a1b043f914ea43b7b97
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-09-02 16:29:04 +0200

    Write deserialization functions based off tests

--- pkg/deserializer.go
+++ pkg/deserializer.go
@@ -1,5 +1,40 @@
 package pkg
 
+import (
+	"strconv"
+	"strings"
+)
+
 func DeserializeSimpleStrings(value string) string {
-	return "OK"
+	value = strings.TrimPrefix(value, SIMPLE_STRING_PREFIX)
+	value = strings.TrimSuffix(value, CRLF)
+
+	return value
+}
+
+func DeserializeErrors(value string) string {
+	value = strings.TrimPrefix(value, ERROR_PREFIX)
+	value = strings.TrimSuffix(value, CRLF)
+
+	return value
+}
+
+func DeserializeIntegers(value string) int {
+	value = strings.TrimPrefix(value, INT_PREFIX)
+	value = strings.TrimSuffix(value, CRLF)
+
+	convertedValue, _ := strconv.Atoi(value)
+
+	return convertedValue
+}
+
+func DeserializeBulkStrings(value string) string {
+	value = strings.TrimPrefix(value, BULK_STRING_PREFIX)
+	value = strings.TrimSuffix(value, CRLF)
+
+	return value[3:] // removes the length in the final string (the number after $)
+}
+
+func DeserializeArrays(value string) interface{} {
+	return []string{"1"}
 }
