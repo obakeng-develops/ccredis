d3a0417d01cbd69a15709a23cc8d94bc03f4b788
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-10-17 13:35:35 +0200

    Start reading data from the connection and write to client:

--- server/server.go
+++ server/server.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"bufio"
 	"fmt"
 	"log/slog"
 	"net"
+	"strings"
 )
 
 type Server struct {
@@ -18,13 +20,13 @@ func (s *Server) NewServer(address string) *Server {
 
 func (s *Server) StartServer() {
 	// Listen on port 6379
-	ln, err := net.Listen("tcp", s.Address)
+	ln, err := net.Listen("tcp", ":6379")
 	if err != nil {
 		slog.Error("Could not listen on given port", "err", err)
 		return
 	}
 	defer ln.Close()
-	fmt.Println("Server is listening on port 6379...")
+	fmt.Printf("Server is listening on port %s...", s.Address)
 
 	// Start accepting connections
 	for {
@@ -42,5 +44,28 @@ func (s *Server) StartServer() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	fmt.Println("Hello")
+	reader := bufio.NewReader(conn)
+	for {
+		data, err := reader.ReadString('\n')
+		if err != nil {
+			slog.Error("Could not read data from connection", "err", err)
+			return
+		}
+
+		message := strings.TrimSpace(data)
+		fmt.Println("Received message: %s", data)
+
+		var response string
+		if message == "*1$4PING" {
+			response = "+PONG\r\n"
+		} else {
+			response = "+OK\r\n"
+		}
+
+		_, err = conn.Write([]byte(response))
+		if err != nil {
+			slog.Error("Could not write to connection", "err", err)
+			return
+		}
+	}
 }
