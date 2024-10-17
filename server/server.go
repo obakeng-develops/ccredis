3c3a9cc8dac581011e41a7a586b6f171b9bddceb
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-10-17 12:49:48 +0200

    Add returns/continue in the correct places + Make Address field public.

--- server/server.go
+++ server/server.go
@@ -1,30 +1,35 @@
 package server
 
 import (
+	"fmt"
 	"log/slog"
 	"net"
 )
 
 type Server struct {
-	address string
+	Address string
 }
 
 func (s *Server) NewServer(address string) *Server {
 	return &Server{
-		address: address,
+		Address: address,
 	}
 }
 
 func (s *Server) StartServer() {
-	ln, err := net.Listen("tcp", s.address)
+	ln, err := net.Listen("tcp", s.Address)
 	if err != nil {
 		slog.Error("An error occurred", "err", err)
+		return
 	}
+	defer ln.Close()
+	fmt.Println("Server is listening on port 6379...")
 
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			slog.Error("An error occurred", "err", err)
+			continue
 		}
 
 		go handleConnection(conn)
