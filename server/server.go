a66743a884b475924e2830142096c14db034770b
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-10-17 12:57:53 +0200

    Add comments + correct error messages on server

--- server/server.go
+++ server/server.go
@@ -17,25 +17,30 @@ func (s *Server) NewServer(address string) *Server {
 }
 
 func (s *Server) StartServer() {
+	// Listen on port 6379
 	ln, err := net.Listen("tcp", s.Address)
 	if err != nil {
-		slog.Error("An error occurred", "err", err)
+		slog.Error("Could not listen on given port", "err", err)
 		return
 	}
 	defer ln.Close()
 	fmt.Println("Server is listening on port 6379...")
 
+	// Start accepting connections
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			slog.Error("An error occurred", "err", err)
+			slog.Error("Could not accept connection", "err", err)
 			continue
 		}
 
+		// Handle the connection in a goroutine
 		go handleConnection(conn)
 	}
 }
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
+
+	fmt.Println("Hello")
 }
